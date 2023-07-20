4ab329ea5bcb1457968e206714ebbc83a82f36dd
navink84 <70619811+navink84@users.noreply.github.com>
2023-07-21 03:27:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,9 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
